b498ae548aee25cc370abee819db445deeea7e91
BNam <namkoi2010@gmail.com>
2026-06-02 10:47:21 +0700

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "QNT_Documentation",
+  description: "Quảng Ninh Travel - Một sản phẩm hỗ trợ cải thiện hệ sinh thái du lịch, tỉnh Quảng Ninh",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Quảng Ninh Travel"
+  text: "Một sản phẩm hỗ trợ cải thiện hệ sinh thái du lịch tỉnh Quảng Ninh"
+  tagline: Thực hiện bởi học sinh trường THPT Cẩm Phả
+  actions:
+    - theme: brand
+      text: Bắt đầu ngay
+      link: /markdown-examples
+    - theme: alt
+      text: Xem mã nguồn (Chỉ dev)
+      link: https://github.com/bnam8210/quang_ninh_travel
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
